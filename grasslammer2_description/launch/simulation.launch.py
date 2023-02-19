6466069bcf27131c1c35de63d29427c390228b75
TeoZeta <mtt.zinzani@gmail.com>
2023-02-19 21:56:50 +0100

    added teleop joy launch working

--- grasslammer2_description/launch/simulation.launch.py
+++ grasslammer2_description/launch/simulation.launch.py
@@ -3,18 +3,18 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess, GroupAction
 from launch.conditions import IfCondition, UnlessCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command, LaunchConfiguration, PythonExpression
-from launch_ros.actions import Node
+from launch_ros.actions import Node, SetRemap
 from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
 
     pkg_path = os.path.join(get_package_share_directory('grasslammer2_description'))
     pkg_path_fre = os.path.join(get_package_share_directory('virtual_maize_field')) # I want to launch the gaz_sim.launch.py that is the launch file for the 2022 FRE environment
-    
+    pkg_path_joy = os.path.join(get_package_share_directory('joystick_ros2'))
 
 
     start_gazebo = IncludeLaunchDescription(
@@ -41,12 +41,18 @@ def generate_launch_description():
                                 ),
                     )
 
-    teleop_node = Node(package='teleop_twist_keyboard',
-                        executable='teleop_twist_keyboard',
-                        output='screen',
-                        remappings=[('/cmd_vel', '/grasslammer_velocity_controller/cmd_vel_unstamped')],
-                    )
     
+    # Control robot using joystick
+    teleop_joy = GroupAction(
+                    actions=[
+
+                        SetRemap(src='/cmd_vel',dst='/grasslammer_velocity_controller/cmd_vel_unstamped'),
+
+                        IncludeLaunchDescription(
+                            PythonLaunchDescriptionSource(pkg_path_joy + '/launch/joystick.launch.py'),
+                        )
+                    ]
+                )           
 
     
 
@@ -55,6 +61,6 @@ def generate_launch_description():
         #start_gazebo_fre,
         start_urdf,
         start_control,
-        teleop_node,
+        teleop_joy,
          
     ])
