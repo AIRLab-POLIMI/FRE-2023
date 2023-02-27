e47f49d45aeee5e9cefc5f9a36fbdfba634d9c0b
TeoZeta <mtt.zinzani@gmail.com>
2023-02-27 13:01:26 +0100

    fix robot model Rviz visualization

--- grasslammer2_description/launch/bot_state_publisher.launch.py
+++ grasslammer2_description/launch/bot_state_publisher.launch.py
@@ -20,7 +20,7 @@ def generate_launch_description():
 
     spawn_x_val = '-1'
     spawn_y_val = '0'
-    spawn_z_val = '.5'
+    spawn_z_val = '0.2'
     spawn_yaw_val = '0.0'#'1.57'
 
     #Create robot state publisher node 
@@ -28,7 +28,7 @@ def generate_launch_description():
     node_robot_state_publisher = Node(
                 package='robot_state_publisher', 
                 executable='robot_state_publisher', 
-                output='screen',
+                #output='screen',
                 parameters=[params]
     )
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
@@ -44,7 +44,7 @@ def generate_launch_description():
                        executable = "static_transform_publisher",
                        arguments = [spawn_x_val,
                                     spawn_y_val,
-                                    spawn_z_val,
+                                    "0.0",
                                     "0.0",
                                     "0.0",
                                     "0.0",
@@ -56,7 +56,7 @@ def generate_launch_description():
                                 )          
     rplidar_node = Node(package='rplidar_ros',
                         executable='rplidar_composition',
-                        output='screen',
+                        #output='screen',
                         parameters=[{
                             'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-port0',
                             'frame_id': 'laser_frame',
@@ -72,5 +72,5 @@ def generate_launch_description():
         spawn_entity, 
         map_odom_static_tf,
         odom_base_footprint_tf,
-        rplidar_node,
+        #rplidar_node,
     ])
