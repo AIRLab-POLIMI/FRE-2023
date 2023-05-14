fba562d5bfa15ef8c6167f456dc912f3c5f2912a
CarloArnone <carlo.arnone@mail.polimi.it>
2023-05-14 14:22:28 +0200

    launch on cust_fre_3

--- grasslammer2_description/launch/gaz_world.launch.py
+++ grasslammer2_description/launch/gaz_world.launch.py
@@ -20,7 +20,7 @@ def generate_launch_description():
     path_gz = os.path.join(get_package_share_directory('gazebo_ros'))
 
     #set path to world file 
-    world_file = 'cust_fre_nav_1.world' # World options are the ones in /worlds folder 
+    world_file = 'cust_fre_nav_3.world' # World options are the ones in /worlds folder 
 
     
 
