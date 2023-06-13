59bd397babd09283eade6d9b14a47cb004fce732
CarloArnone <carlo.arnone@mail.polimi.it>
2023-06-13 09:56:13 +0200

    parser correct

--- grasslammer2_nav_py/grasslammer2_nav_py/definitive_turner.py
+++ grasslammer2_nav_py/grasslammer2_nav_py/definitive_turner.py
@@ -16,16 +16,21 @@ class TurnerFinal(Node):
     def __init__(self):
         super().__init__('turner_final')
         self.lineDimension = 0.75
-        self.y_movement = 0.20
-        self.turnNum = 0;
+        self.y_movement = -0.6
+        self.turnNum = 0
         self.side = "right"
         self._tf_buffer = Buffer()
+        self.turningInput = None
+        self.turningCommands = []
         self._tf_listener = TransformListener(self._tf_buffer, self)
         self.starting_pose_sub = self.create_subscription(PoseStamped, '/end_of_line_pose', self.elaborate_goal_point, 1)
         self.done = self.create_publisher(Bool, '/end_of_turning', 1)
-        pkg_path = os.path.realpath("src/grasslammer2/grasslammer2_description")
+        self.pose = self.create_publisher(PoseStamped, '/nav2_goal_pose', 1)
+        pkg_path = os.path.realpath("src/FRE-2023/grasslammer2_description")
         with open(pkg_path + "/config/pathTask1.txt") as path:
-            self.turningCommands = path.readlines()
+            self.turningInput = path.readline().split(" - ")
+        for i in range(1, len(self.turningInput)):
+            self.turningCommands.append([self.turningInput[i][:-1], self.turningInput[i][-1]])
         print(self.turningCommands)
         self.navigator = BasicNavigator()
     
